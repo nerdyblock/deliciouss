2ce904377c62726ff3e876830594cfd49f1750a1
Akash Pal <creativeakashpal@gmail.com>
2023-07-25 20:33:55 +0530

    added basic pages

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Popular from "../components/Popular";
+import Veggies from "../components/Veggies";
+
+export default function Home() {
+  return (
+    <>  
+        <Popular/>
+        <Veggies/>
+    </>
+  )
+}
