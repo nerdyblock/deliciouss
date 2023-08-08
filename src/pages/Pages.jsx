76c3315077838b5aaa7f2ab0e3fa5f5686c793fb
Akash Pal <creativeakashpal@gmail.com>
2023-08-08 12:26:02 +0530

    added routes

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,7 +1,21 @@
+import Cuisine from "./Cuisine";
 import Home from "./Home";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Searched from "./Searched";
+import Recipe from "./Recipe";
+import { AnimatePresence } from "framer-motion";
 
 export default function Pages() {
+  const location = useLocation();
+
   return (
-    <Home />
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/cuisine/:type" element={<Cuisine/>}/>
+        <Route path="/searched/:search" element={<Searched />} />
+        <Route path="/recipe/:name" element={<Recipe />} />
+      </Routes>
+    </AnimatePresence>
   )
 }
