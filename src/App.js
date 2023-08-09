474045044ec4884f0be17fad4350c7e94d4700b3
Akash Pal <creativeakashpal@gmail.com>
2023-08-09 11:27:48 +0530

    added routes and styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import { BrowserRouter, Link } from "react-router-dom";
+import Category from "./components/Category";
 import Pages from "./pages/Pages";
+import Search from "./components/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
-      <Pages/>
+      <BrowserRouter >
+        <Nav>
+          <GiKnifeFork/>
+          <Logo to={"/"} >Deliciouss</Logo>
+        </Nav>
+        <Search />
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-weight: 400;
+  font-size: 1.4rem;
+  font-family: 'Lobster Two', cursive;
+`
+
+const Nav = styled.div`
+  padding: 2rem 0;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  svg {
+    font-size: 2rem;
+  }
+`
 
 export default App;
